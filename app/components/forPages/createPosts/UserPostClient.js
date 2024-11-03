911b70bdcd92265964ba9e5508a824b0543f4d16
AidanAcartis <ralaikotonadia@gmail.com>
2024-11-03 23:12:58 +0300

    to push on an another brach

--- app/components/forPages/createPosts/UserPostClient.js
+++ app/components/forPages/createPosts/UserPostClient.js
@@ -53,6 +53,11 @@ const PostCard = ({ post }) => {
         window.location.reload();
     };
 
+    // Fonction pour extraire le nom du fichier depuis l'URL
+   const extractFileName = (url) => {
+    return url.split('/').pop();
+  };
+
     return (
         <Card>
             <div className="flex gap-3">
@@ -82,12 +87,6 @@ const PostCard = ({ post }) => {
                         </button>
                         {menuOpen && (
                             <div className="absolute -right-6 py-3 w-48 bg-white rounded-sm shadow-md shadow-gray-300 border border-gray-100">
-                                <a href="/saved" className="flex px-2 py-2 gap-2 text-gray-800 hover:bg-socialBlue hover:text-white rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
-                                    </svg>
-                                    Save post
-                                </a>
                                 <a 
                                     href="#" 
                                     className="flex px-2 py-2 gap-2 text-gray-800  hover:bg-socialBlue hover:text-white rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300"
@@ -97,6 +96,12 @@ const PostCard = ({ post }) => {
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                     </svg>
                                     Delete
+                                </a>
+                                <a href="/saved" className="flex px-2 py-2 gap-2 text-gray-800 hover:bg-socialBlue hover:text-white rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
+                                    </svg>
+                                    Save post
                                 </a>
                                 <a href="#" className="flex px-2 py-2 gap-2 text-gray-800 hover:bg-socialBlue hover:text-white rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -135,7 +140,7 @@ const PostCard = ({ post }) => {
                     )}
                     {post.doc_type === 'pdf' && (
                         <a href={post.doc_url} target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
-                            Ouvrir le document PDF
+                            {extractFileName(post.doc_url)}
                         </a>
                     )}
                 </div>
